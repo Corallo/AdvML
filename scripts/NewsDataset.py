894b5a5c50285f802407ff9851012402f7fe232d
FlaviaGarcia <flavi13a@gmail.com>
2020-01-09 17:09:53 +0100

    changes in tfidf, needed target change

--- scripts/NewsDataset.py
+++ scripts/NewsDataset.py
@@ -1,30 +1,31 @@
 import numpy as np
-import scipy.sparse as sp
 import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import SGDClassifier
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
-from sklearn.naive_bayes import MultinomialNB
+
 from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import TfidfTransformer
-import pandas as pd
 
 import nltk
-nltk.download('punkt')
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
 
 #subset: choose between train, test or all
-#category: for example comp.os.ms-windows.misc
+#categories: list of wanted categories 
 #dimensions: number of relevant words to use
 #plot_tf_idf: if true plot the tf-idf summed value for each word
-def get_20newsgroup_tf_idf(subset, category, dimensions, plot_tf_idf = False):
-
-    newsgroups_set = fetch_20newsgroups(subset='all', categories=[category])
+def get_20newsgroup_tf_idf(subset, categories, dimensions, plot_tf_idf = False):
+    newsgroups_set = fetch_20newsgroups(subset='all', categories=categories)
     newsgroups_set.data = list(set(newsgroups_set.data))
-    stop_words = set(stopwords.words('english'))
+    stop_words = stopwords.words('english')
     vectorizer = TfidfVectorizer(stop_words = stop_words)
     vectors = vectorizer.fit_transform(newsgroups_set.data)
     ordered_tfidf = np.sort(np.sum(vectors.toarray(), axis = 0))[::-1]
@@ -36,7 +37,7 @@ def get_20newsgroup_tf_idf(subset, category, dimensions, plot_tf_idf = False):
     most_importan_words_index = list(map(lambda a : a[0], most_importan_words_indexvalues))
     most_importan_words = np.array(vectorizer.get_feature_names())[most_importan_words_index]
     words_to_remove = list(set(vectorizer.get_feature_names()) - set(most_importan_words))
-    vectorizer = TfidfVectorizer(stop_words = (list(stop_words) + words_to_remove))
+    vectorizer = TfidfVectorizer(stop_words = (stop_words + words_to_remove))
     vectors = vectorizer.fit_transform(newsgroups_set.data)
 
     matrix_tf_idf = vectors.toarray()
@@ -47,8 +48,7 @@ def get_20newsgroup_tf_idf(subset, category, dimensions, plot_tf_idf = False):
         X = np.arange(0, len(ordered_tfidf), 1)
         plt.plot(X, ordered_tfidf)
         plt.show()
-    return matrix_tf_idf
+        
+    return matrix_tf_idf, targets, newsgroups_set.target_names
 
-
-print(get_20newsgroup_tf_idf("all", "comp.os.ms-windows.misc", 7511, plot_tf_idf = True))
-#get_20newsgroup("all", "comp.sys.mac.hardware ")
+newsgroup_tf_idf, targets, targets_ids = get_20newsgroup_tf_idf("all", ["comp.os.ms-windows.misc", "comp.sys.mac.hardware"], 7511)
